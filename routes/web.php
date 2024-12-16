9f00641d2baf1447206426203bfbbce8fe1e1193
fatmahamdy188 <143092084+fatmahamdy188@users.noreply.github.com>
2024-12-16 04:31:05 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,12 +44,6 @@ Route::get('/tasks/{task}', function (Task $task) {
 
 Route::post('/tasks', function(TaskRequest $request) {
   
-    /*$data= $request->validated();
-    $task= new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();*/
     $task = Task::create($request->validated());
     return redirect()->route('tasks.show', ['task'=> $task->id])->with('success','Task created successfully');
 })->name('tasks.store');
@@ -57,11 +51,6 @@ Route::post('/tasks', function(TaskRequest $request) {
 
 Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
    
-    /*$data= $request->validated();
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();*/
     $task->update($request->validated());
     return redirect()->route('tasks.show', ['task'=> $task->id])->with('success','Task updated successfully');
 })->name('tasks.update');
